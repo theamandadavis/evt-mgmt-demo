78768b19eda17c0edf025276d7e8678cabfa4b2f
theamandadavis <davisamanda599@gmail.com>
2026-03-27 20:32:27 -0400

    forces dynamic

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,5 +1,7 @@
 import { EventVenueList } from "@/components/event-venue-list";
 
+export const dynamic = "force-dynamic";
+
 export default function ProtectedPage() {
   return <EventVenueList />;
 }
