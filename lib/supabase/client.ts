db76558b78600826df8f34663e1567ce7b383912
theamandadavis <davisamanda599@gmail.com>
2026-03-27 22:19:14 -0400

    envs will be the death of me

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -3,7 +3,7 @@ import type { Database } from "./database.types";
 
 export function createClient() {
   return createBrowserClient<Database>(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
 }
